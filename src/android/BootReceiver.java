7f3fc0cc63635e365edda4fb2085fe25afb2cb61
Pablo Ortiz <portizb@gmail.com>
2016-10-13 21:45:24 +0200

    Update BootReceiver.java

--- src/android/BootReceiver.java
+++ src/android/BootReceiver.java
@@ -14,20 +14,7 @@ public class BootReceiver extends BroadcastReceiver {
 	@Override  
 	public void onReceive(Context context, Intent intent) {
 		
-		// Get all the registered and loop through and start them
-		String[] serviceList = PropertyHelper.getBootServices(context);
-		
-		if (serviceList != null) {
-			for (int i = 0; i < serviceList.length; i++)
-			{
-				// Fix to https://github.com/Red-Folder/bgs-core/issues/18
-				// Gets the class from string
-				Class<?> serviceClass = ReflectionHelper.LoadClass(serviceList[i]);
-
-				Intent serviceIntent = new Intent(context, serviceClass);         
-				context.startService(serviceIntent);
-			}
-		}
+		Intent serviceIntent = new Intent(context, TvServices.class);         
+		context.startService(serviceIntent);
 	} 
-	
 } 
